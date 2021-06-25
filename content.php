82b969ce903a8b038325a1bc4b627082b62cbb0d
PiosX <piosso.pios@op.pl>
2021-06-25 22:29:25 +0200

    Users Online check unc.

--- content.php
+++ content.php
@@ -7,6 +7,7 @@
         session_destroy();     
         header("location:index.php");
     }
+    $login = $_SESSION['login'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -45,7 +46,7 @@
             <tbody>
                 <tr>
                     <?php
-                    $login = $_SESSION['login'];
+                    
                         $stmt = $dbh->prepare("SELECT users.login, silver, gold, iron, wood, stone FROM resources INNER JOIN users ON users.id = resources.user_id WHERE users.login = :login");
                 
                         $stmt->bindParam(":login", $login);
@@ -62,18 +63,45 @@
                             echo "<td>".$row['stone']."</td>";
                         }
                         }
-
+                        $stmt->closeCursor();
                     ?>
                 </tr>
             </tbody>
         </table>
+        <h3>Online Users:</h3>
+            <?php
+                if($_SESSION['logged'] == 1)
+                {
+                    $stmt = $dbh->prepare("SELECT * FROM online_users WHERE login = '$login'");
+                    $stmt->execute();
+
+                    if($stmt->rowCount() == 0)
+                    {
+                        $stmt = $dbh->prepare("INSERT INTO online_users(login) VALUES('$login')");
+
+                        $stmt->execute();
+                    }
+                    
+                    $stmt = $dbh->prepare("SELECT login FROM online_users");
+
+                    $stmt->execute();
+                    
+                    if($stmt->rowCount()>0)
+                    {
+                        while($row = $stmt->fetch())
+                        {
+                            echo $row['login'].", ";
+                        } 
+                    }
+                    
+                }
+            ?>
     </div>
     <div id="bottom-container">
         <p>
             <?php
                 require_once("login.php");
-                $login = $_SESSION['login'];
-                
+                         
                 $stmt = $dbh->prepare("SELECT register_date FROM users WHERE login = '$login'");
                 
                 $stmt->execute();
@@ -82,6 +110,7 @@
                 {
                     echo "Your register date: ".$row['register_date'];
                 }
+                $stmt->closeCursor();
             ?>
         </p>
     </div>
