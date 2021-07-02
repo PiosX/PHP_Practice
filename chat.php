abc87d7eb9d199f6f5478407a6e0029d37d7aa86
PiosX <piosso.pios@op.pl>
2021-07-02 10:47:03 +0200

    Chat

--- chat.php
+++ chat.php
@@ -1,8 +1,18 @@
 <?php
     session_start();
     require_once("dbh_connection.php");
+    require_once("checkUser.php");
     $login = $_SESSION['login'];
     $log = $_GET['profile'];
+
+    if(isset($_GET['profile']) && $_GET['profile'] == $login)
+    {
+        header("Location:profile.php?profile=$login");
+    }
+    if(isset($_GET['profile']) && $_GET['profile'] == '')
+    {
+        header("Location:profile.php?profile=$login");
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -35,14 +45,22 @@
                     }else{
                         echo "Not Found.";
                     }
-                    $stmt = $dbh->prepare("SELECT message FROM messages WHERE send_by = '$login' AND send_to = '$log'");
+                    $stmt = $dbh->prepare("SELECT send_by, send_to, message FROM messages WHERE (send_by = '$login' AND send_to = '$log') OR (send_by = '$log' AND send_to = '$login')");
                     $stmt->execute();
                     if($stmt->rowCount()>0)
                     {
                         while($row = $stmt->fetch())
                         {
-                            echo "<div class='msg-box'>".$row['message']."</div>";
+                            if($row['send_by'] == $login)
+                            {
+                                echo "<div class='msg-box'>".$row['message']."</div>";
+                            }
+                            else
+                            {
+                                echo "<div class='msg-box-2'>".$row['message']."</div>";
+                            }
                         }
+                        
                     }
                                    
                 }
